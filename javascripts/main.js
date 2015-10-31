50b21cadbed7062968c9111943c349ec5654cf00
Tom Taylor <theetommyt@gmail.com>
2015-10-31 12:42:48 -0500

    angular

---
+++ javascripts/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+
+  $('.parallax').parallax();
+
+  $(".button-collapse").sideNav();
+
+  $('.modal-trigger').leanModal();
+
+
+
+///////     Smooth Scroll     ///////
+
+  $('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top - 60
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
+
+///////     jQuery Animations     ///////
+
+$('.contain').fadeIn(3500);
+
+
+
+});
---
+++ todo.md
@@ -0,0 +1,20 @@
+ # ToDo List
+
+- Include tech used in each project
+- fix wave effect on links for cards
+- Add slider with work ex in startups
+- Founder/President position on startups
+- press links startups
+- mission statements startups
+- blue color parallax backgrounds
+- fix reign on heroku (fonts and login)
+- cleanup CYOD
+- finish Charity Race
+- finish PPS
+- web for races go public?
+- optimize images for web
+- fix hamburger menu wave effect
+
+
+
+## Saved Code
